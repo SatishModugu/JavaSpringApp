af0250a4ea20a15a605d756bccc4b84227d117f1
SatishModugu <32131478+SatishModugu@users.noreply.github.com>
2019-03-24 23:07:34 -0500

    Edits

--- spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/luv2code/springdemo/CricketCoach.java
@@ -3,7 +3,26 @@ package com.luv2code.springdemo;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	
+	//add new fields for emailAdress and team;
+	private String emailAddress;
+	private String team;
+	
 
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAdfress) {
+		System.out.println("Inside the setter method - Email Adress");
+		this.emailAddress = emailAdfress;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		System.out.println("Inside the setter method - SetTeam");
+		this.team = team;
+	}
 	@Override
 	public String getDailyWorkout() {
 		// TODO Auto-generated method stub
@@ -16,6 +35,7 @@ public class CricketCoach implements Coach {
 	//Creating Setter Method 
 	public void setFortuneService(FortuneService fortuneService) {
 		this.fortuneService = fortuneService;
+		System.out.println("Inside the setter method");
 	}
 	@Override
 	public String getDailyFortune() {
